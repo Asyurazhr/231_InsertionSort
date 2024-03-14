5d4c195d2c7091ae7c4e765c8e1c04156bfe731a
Asyurazhr <Asyuradjola2004@gmail.com>
2024-03-14 15:37:40 +0700

    membuat step 1

--- PertemuanKeempat/PertemuanKeempat.cpp
+++ PertemuanKeempat/PertemuanKeempat.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 using namespace std;
+int arr[40];
+int n;
 
-void InsertionSort() {
-    int i, j, n, temp;
-    int arr[40];
+
+void input() {
+    while (true) {
+        cout << "Masukkan banyaknya elemen pada array: ";
+        cin >> n;
+        if (n <= 40)
+            break;
+        else {
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+        }
+    }
+    cout << endl;
+    cout << "===============" << endl;
+    cout << "Masukkan elemen array: " << endl;
+    cout << "===============" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "Data ke-" << (i + 1) << ":";
+        cin >> arr[i];
+    }
+}
+
+void insertionSort() {
+    int i, j, temp;
 
     for (i = 1; i <= n - 1; i++) { //step 1
         temp = arr[i]; //step 2
@@ -22,7 +45,24 @@ void InsertionSort() {
     }
 }
 
+void display() {
+    cout << endl;
+    cout << "==============" << endl;
+    cout << "Elemen array yang telah tersusun" << endl;
+    cout << "==============" << endl;
+    for (int j = 0; j < n; j++) {
+        cout << arr[j] << endl;
+    }
+    cout << "Jumlah pass = " << n - 1 << endl;
+    cout << endl;
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    input();
+    insertionSort();
+    display();
+
+    system("Pause");
+    return 0;
 }
+
